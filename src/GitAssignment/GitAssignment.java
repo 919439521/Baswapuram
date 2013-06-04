bb35cbac08c92c0c02fcb9ffe1d99cf1d4977c62
S516466 <S516466@NW23042.nwmissouri.edu>
2013-06-04 11:27:48 -0500

    I've added a print statement (Satish Kumar Baswapuram).

---
+++ src/GitAssignment/GitAssignment.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GitAssignment;
+
+/**
+ *
+ * @author S516466
+ */
+public class GitAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("\nSatish Kumar Baswapuram");
+    }
+}
